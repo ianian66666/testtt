77a550d4ca0851807a2e6e21c3dd26394395a8f9
ian <ian1020304@gmail.com>
2020-04-07 15:03:44 +0800

    Initial commit

---
+++ src/com/ian/HelloWorld.java
@@ -0,0 +1,31 @@
+package com.ian;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        getlist();
+         System.out.println("helloworld");
+
+        List<String> list = new ArrayList<>();
+
+                list.add("ada");
+
+
+    }
+
+    public static void getlist() {
+        System.out.println("helloworld");
+        int ss = 10;
+        System.out.println("helloworld1");
+        Date date = new Date();
+    }
+
+    public static void ms() {
+        int sss = 25;
+    }
+}
---
+++ src/com/ian/hello.java
@@ -0,0 +1,25 @@
+package com.ian;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ian
+ * @date 2020/4/7 -  下午 01:02
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        //模板2
+        System.out.println("hello");
+        //iter
+        for (String arg : args) {
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < list.size(); i++) {
+                String s = args[i];
+                
+            }
+            list.for
+        }
+    }
+}
