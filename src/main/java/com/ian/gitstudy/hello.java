d3dfe2ebe82511f4464489e3eab13a3fefbc0d01
ian <ian1020304@gmail.com>
2020-05-20 15:12:47 +0800

    第二次提交

---
+++ src/main/java/com/ian/gitstudy/hello.java
@@ -0,0 +1,11 @@
+package com.ian.gitstudy;
+
+/**
+ * @author Ian
+ * @date 2020/5/20 -  下午 03:11
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
